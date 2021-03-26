1bab201b5838567074618e77bfc5b9462afdd780
Subham <subham.sinha41@gmail.com>
2021-03-26 22:14:37 +0530

    second commit

---
+++ src/TodoForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import useInputState from './useInputState';
+import './ToDo.css';
+
+const TodoForm = ({ saveTodo }) => {
+  const { value, reset, onChange } = useInputState();
+
+  return (
+    <form className = "todo"
+      onSubmit={event => {
+        event.preventDefault();
+
+        saveTodo(value);
+        reset();
+      }}
+    >
+      <TextField
+        variant="outlined"
+        placeholder="Add Item"
+        margin="normal"
+        onChange={onChange}
+        value={value}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default TodoForm;
